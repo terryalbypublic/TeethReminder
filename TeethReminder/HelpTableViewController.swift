3b01f70133c4f3dd6ce56f9bfc9ade39ac26fa7a
Terence Alberti <terence.alberti@xmedia.ch>
2015-12-13 23:53:19 +0100

    fix UI

--- TeethReminder/HelpTableViewController.swift
+++ TeethReminder/HelpTableViewController.swift
@@ -1,47 +1,23 @@
 //
-//  RemindersTableViewController.swift
+//  HelpTableViewController.swift
 //  TeethReminder
 //
-//  Created by Alberti Terence on 07/12/15.
+//  Created by Alberti Terence on 13/12/15.
 //  Copyright © 2015 TA. All rights reserved.
 //
 
 import UIKit
 
-class RemindersTableViewController: UITableViewController {
+class HelpTableViewController: UITableViewController {
 
-    
-    var reminderList = ReminderList.sharedInstance
-    var reminders = Array<Reminder>()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.SingleLine
-        //self.tableView.separatorColor = UIColor.blueColor()
-        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
-        fillReminders()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(false)
-        self.tableView.reloadData()
-    }
-    
-    func fillReminders(){
-        reminderList.deserialize()
-        reminders = reminderList.reminders
-    }
-    
-    func setReminderActive(isActive: Bool, index: Int){
-        reminders[index].isActive = isActive
-        reminderList.serializeAndSave()
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,24 +34,30 @@ class RemindersTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return reminders.count
+        return 2
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell : TableViewCell = tableView.dequeueReusableCellWithIdentifier("ReminderCell", forIndexPath: indexPath) as! TableViewCell
-
-        cell.index = indexPath.row
-        cell.name.text = reminders[indexPath.row].name
-        cell.isActive.setOn(reminders[indexPath.row].isActive, animated: false)
-        cell.time.text = reminders[indexPath.row].time()
         
-        // Configure the cell...
+        if(indexPath.row == 0){
+            return tableView.dequeueReusableCellWithIdentifier("helpIdentifier", forIndexPath: indexPath)
+            
+        }
+        else{
+            return tableView.dequeueReusableCellWithIdentifier("backButtonIdentifier", forIndexPath: indexPath)
+        }
+        
+    }
 
-        return cell
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if(indexPath.row == 1){
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
     }
     
-
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
@@ -111,26 +93,14 @@ class RemindersTableViewController: UITableViewController {
     }
     */
 
-    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        
-        if(self.tableView.indexPathForSelectedRow != nil){
-            
-            let rowIndex = self.tableView.indexPathForSelectedRow?.row
-            
-            let reminder = ReminderList.sharedInstance.reminders[rowIndex!]
-            
-            let nextViewController = segue.destinationViewController as! SetTimeViewController
-            nextViewController.reminder = reminder
-            
-        }
-        
     }
-    
+    */
 
 }
